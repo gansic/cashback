3877fbb2529fb47941b2eb5903f2dd740e08b403
gansic <rus.com.rf@gmail.com>
2020-10-10 23:07:54 +0300

    initial

---
+++ src/BankService.java
@@ -0,0 +1,14 @@
+public class BankService {
+    int calculate(int point) {
+        // 1 bal = 1 rub
+        // 3000 rub
+        int win = 3000;
+        if (point == win) {
+            return win;
+        }
+        int credit = 1;
+        boolean profit = point == credit ;
+        return point;
+    }
+}
+
---
+++ src/main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+        BankService cashback = new BankService();
+        int result = cashback.calculate(5000);
+        System.out.println(result);
+
+
+    }
+}
